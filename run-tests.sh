23de9c559f28acebb739f8049145fd7b5439df9f
John Dzak <john.dzak@gmail.com>
2011-04-12 00:35:15 -0500

    Dynamic test build/running.

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 make
 
+TEST_DIR="test/build"
 if [ $? -eq 0 ] ; then
-  for f in test/target/*test
-  do
-      echo "Running test - $f"
-      exec $f
-  done
+  if [$1 -n]; then
+    exec $TEST_DIR/$1
+  else
+    for f in $TEST_DIR/*test
+    do
+        echo "Running test - $f"
+        exec $f
+    done
+  fi
 fi
--- test/sample1_unittest.cc
+++ test/sample1_unittest.cc
@@ -44,7 +44,7 @@
 // Don't forget gtest.h, which declares the testing framework.
 
 #include <limits.h>
-#include "../main/sample1.h"
+#include "../src/sample1.h"
 #include <gtest/gtest.h>
 
 
